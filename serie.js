3c1ec0a1aa79746c932a7b928e8cf11441fa8fc3
LeFilgueiras <filgueiras.leticiac@gmail.com>
2017-02-16 10:47:30 -0300

    JSON

---
+++ serie.js
@@ -0,0 +1,183 @@
+series = {
+ "twd": {
+  imagem: "the-walking-dead-4-temporada-parte-2-poster.jpg",
+  serie: "The Walking Dead",
+  criado_por: "Frank Darabont",
+  elenco: "Norman Reedus, Andrew Lincoln, Steven Yeun",
+  pais: "EUA",
+  genero: "Terror, Suspense",
+  status: "Em produção",
+  duracao: "42 minutos",
+  n_temporadas: "7",
+  emissora: "AMC",
+  sinopse: "Este drama retrata a vida num Estados Unidos pós-apocalíptico. Um grupo de sobreviventes - liderado pelo policial Rick Grimes, sua família e outros - segue viajando à procura por uma nova moradia segura e distante dos mortos vivos. E, a pressão para permanecerem vivos e lutarem pela sobrevivência faz com que muitos do grupo participem das mais profundas formas de crueldade humana. Rick acaba descobrindo que o tão assustador medo dos sobreviventes pode ser ainda mais fatal do que os próprios mortos vivos que os rodeiam.",
+},
+ "soa": {
+  imagem: "feb4be4170078a1bdc69f2c856202ec1.jpg",
+  serie: "Sons of Anarchy",
+  criado_por: "Kurt Sutter",
+  elenco: "Charlie Hunnam, Katey Sagal, Kim Coates",
+  pais: "EUA",
+  genero: "Drama, Policial",
+  status: "Finalizada",
+  duracao: "42 minutos",
+  n_temporadas: "7",
+  emissora: "FX",
+  sinopse: "Em Charming, uma pequena cidade fictícia no norte da Califórnia, nos Estados Unidos, habita o clube de motociclistas Sons of Anarchy. O grupo lida com atividades ilícitas como o tráfico de drogas e filmes pornô e é liderado por Clay Morrow (Ron Perlman) e Jax Teller (Charlie Hunnam). Além do esforço para conciliar a sua ocupação com a família e os filhos, eles tem de conviver com suas opiniões diferentes: enquanto Morrow opta pela tradição violenta, Teller se inspira no discurso revolucionário do diário de seu pai falecido0.",
+},
+ "scream":{
+  imagem: "jj.png",
+  serie: "Scream",
+  criado_por: "Dan Dworkin, Jay Beattie",
+  elenco: "Willa Fitzgerald, John Karna, Tom Maden",
+  pais: "EUA",
+  genero: "Drama, Suspense",
+  status: "Em Produção",
+  duracao: "42 minutos",
+  n_temporadas: "2",
+  emissora: "Netflix, MTV",
+  sinopse: "Depois de um incidente de cyber-bullying resultar em um assassinato brutal, a violência reacende a memória de uma serie de assassinatos que ocorreram no passado em Lakewood, que intrigaram alguns e talvez tenham inspirado um novo serial killer. Um grupo de adolescentes, com dois velhos amigos tentando se reconectarem, se tornam amantes, inimigos, suspeitos, alvos e vítimas de um assassino que está à procura de sangue.",
+},
+ "pll":{
+  imagem: "pll.png",
+  serie: "Pretty Little Liars",
+  criado_por: "I. Marlene King",
+  elenco: "Troian Bellisario, Ashley Benson, Lucy Hale",
+  pais: "EUA",
+  genero: "Drama",
+  status: "Em Produção",
+  duracao: "42 minutos",
+  n_temporadas: "7",
+  emissora: "FreeForm",
+  sinopse: "Na pequena cidade de Rosewood, na Pensilvânia, as melhores amigas Spencer Hastings (Troian Bellisario), Aria Montgomery (Lucy Hale), Hanna Marin (Ashley Benson) e Emily Fields (Shay Mitchell) guardam complexos segredos. Alison (Sasha Pieterse), 5ª membra e líder do grupo, desapareceu há um ano e não deixou qualquer pista. As amigas recebem cartas misteriosas assinadas por uma letra e suspeitam que seja sua antiga companheira, mas a desconfiança que possuem em relação ao universo perverso no qual vivem provoca uma reflexão cada vez maior.",
+},
+ "ga":{
+  imagem: "greys-main.jpg",
+  serie: "Gray's Anathomy",
+  criado_por: "Ellen Pompeo, Justin Chambers, Chandra Wilson",
+  elenco: "Shonda Rhimes",
+  pais: "EUA",
+  genero: "Drama",
+  status: "Em Produção",
+  duracao: "42 minutos",
+  n_temporadas: "13",
+  emissora: "American Broadcasting Company",
+  sinopse: "Meredith Grey (Ellen Pompeo) começa a trabalhar no Seattle Grace Hospital e logo descobre que passou a noite com um dos seus chefes, Dr. Derek Shepherd (Patrick Dempsey). Enquanto enfrenta os desafios da vida profissional, ela se aproxima dos outros internos liderados pela residente Dra. Bailey (Chandra Wilson): Cristina Yang (Sandra Oh), Izzie Stevens (Katherine Heigl), George O'Malley (T.R. Knight) e Alex Karev (Justin Chambers).",
+},
+ "htgawm":{
+  imagem: "howtoget.jpg",
+  serie: "How to Get Away With Murder",
+  criado_por: "	Peter Nowalk",
+  elenco: "Viola Davis, Billy Brown (II), Alfred Enoch",
+  pais: "EUA",
+  genero: "Drama",
+  status: "Em Produção",
+  duracao: "42 minutos",
+  n_temporadas: "3",
+  emissora: "American Broadcasting Company",
+  sinopse: "Michaela, Wes, Laurel e Patrick são ambiciosos calouros de Direito da prestigiada academia East Coast Law School, onde apenas os melhores alunos podem participar de casos reais. Eles competem entre si para conseguir a atenção da carismática e sedutora Professora Annalise DeWitt (Viola Davis), na aula de Direito Criminal 1, também conhecida como 'Como Se Livrar de Um Assassinato'.",
+},
+ "got":{
+  imagem: "game-of-thrones-poster-300x444.jpg",
+  serie: "Game of Thrones",
+  criado_por: "D.B. Weiss, David Benioff",
+  elenco: "Emilia Clarke, Sophie Turner, Kit Harington",
+  pais: "EUA",
+  genero: "Fantsia, Drama",
+  status: "Em Produção",
+  duracao: "60 minutos",
+  n_temporadas: "6",
+  emissora: "HBO",
+  sinopse: "Há muito tempo, em um tempo esquecido, uma força destruiu o equilíbrio das estações. Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, as reivindicações e as forças sobrenaturais correm as portas do Reino dos Sete Reinos. A irmandade da Patrulha da Noite busca proteger o reino de cada criatura que pode vir de lá da Muralha, mas já não tem os recursos necessários para garantir a segurança de todos. Depois de um verão de dez anos, um inverno rigoroso promete chegar com um futuro mais sombrio. Enquanto isso, conspirações e rivalidades correm no jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto.",
+},
+ "reign":{
+  imagem: "c63ea755c4c34e3c0aa225582455452cb7aca2ca444053281c47ef709e441fc2.jpg",
+  serie: "Reign",
+  criado_por: "Laurie McCarthy, Stephanie Sengupta",
+  elenco: "Adelaide Kane, Megan Follows, Rachel Skarsten",
+  pais: "EUA",
+  genero: "Drama, Histórico",
+  status: "Em produçao",
+  duracao: "42 minutos",
+  n_temporadas: "4",
+  emissora: " The CW Television Network",
+  sinopse: "Conta a história da ascenção de Mary Queen of Scots (Adelaide Kane) ao poder quando ela chega à França aos 15 anos de idade, prometida ao Príncipe Francis, e com suas três melhores amigas como damas-de-companhia. Os detalhes da história secreta da sobrevivência na Corte Francesa, em meio a forças ameaçadoras e muitas intrigas.",
+},
+ "tahm":{
+  imagem: "Two_and_a_Half_Men_(1ª_temporada).jpg",
+  serie: "Two and a Half Men",
+  criado_por: "Chuck Lorre, Lee Aronsohn",
+  elenco: "Ashton Kutcher, Jon Cryer, Holland Taylor",
+  pais: "EUA",
+  genero: "Comédia",
+  status: "Finalizada",
+  duracao: "22 minutos",
+  n_temporadas: "12",
+  emissora: "CBS",
+  sinopse: "A vida de um homem rico e solteiro, Charlie (Charlie Sheen), que se aborrece quando seu irmão divorciado Alan (Jon Cryer) e seu sobrinho Jake (Angus T. Jones) acabam indo morar na sua casa em Malibu. Apesar das enormes diferenças entre os dois irmãos, eles decidem firmar uma parceria para dar um lar ao pequeno Jake. ",
+},
+ "friends":{
+  imagem: "MV5BMTg4NzEyNzQ5OF5BMl5BanBnXkFtZTYwNTY3NDg4._V1._CR24,0,293,443_UY1200_CR82,0,630,1200_AL_.jpg",
+  serie: "Friends",
+  criado_por: "Marta Kauffman, David Crane",
+  elenco: "Jennifer Aniston, Courteney Cox, Lisa Kudrow",
+  pais: "EUA",
+  genero: "Comédia",
+  status: "Finalizada",
+  duracao: "22 minutos",
+  n_temporadas: "10",
+  emissora: "NBC",
+  sinopse: "Seis jovens são unidos por laços familiares, românticos e, principalmente, de amizade, enquanto tentam vingar em Nova York. Rachel é a garota mimada que deixa o noivo no altar para viver com a amiga dos tempos de escola Monica, sistemática e apaixonada pela culinária. Monica é irmã de Ross, um paleontólogo que é abandonado pela esposa, que descobriu ser lésbica. Do outro lado do corredor do apartamento de Monica e Rachel, moram Joey, um ator frustrado, e Chandler, de profissão misteriosa. A turma é completa pela exótica Phoebe.",
+},
+ "pb":{
+  imagem: "prisonbreak.jpg",
+  serie: "Prison Break",
+  criado_por: "Paul T. Scheuring",
+  elenco: "Wentworth Miller, Dominic Purcell, Augustus Prew",
+  pais: "EUA",
+  genero: "	Ação, Drama, Suspense",
+  status: "Em produção",
+  duracao: "42 minutos",
+  n_temporadas: "5",
+  emissora: "Fox",
+  sinopse: "Após a prisão de Lincoln Burrows (Dominic Purcell), condenado por um crime que não cometeu, o engenheiro Michael Scofield (Wentworth Miller) bola um plano para tirar o irmão da cadeia. Enviado para Fox River ao lado de Lincoln, Michael começa a executar a sua estratégia, mas logo percebe que está no meio de uma perigosa conspiração. Para garantir a liberdade da sua família, ele precisará enganar a Dra. Sara Tancredi (Sarah Wayne Callies) e se associar a criminosos condenados, como Fernando Sucre (Amaury Nolasco), Theodore 'T-Bag' Bagwell (Robert Knepper) e John Abruzzi (Peter Stormare).",
+},
+ "oitnb":{
+  imagem: "cf604f6b58c09b0a783ba343ca9c34af.jpg",
+  serie: "Orange is The New Black",
+  criado_por: "Jenji Kohan",
+  elenco: "<a href='www.google.com.br'>Taylor Schilling</a>, Jason Biggs, Kate Mulgrew",
+  pais: "EUA",
+  genero: "Drama, Comédia",
+  status: "Em Produção",
+  duracao: "60 minutos",
+  n_temporadas: "4",
+  emissora: "Netflix",
+  sinopse: "Piper Chapman (Taylor Schilling) é uma mulher por volta de seus 30 anos que é sentenciada a 15 meses de prisão após ter cometido crimes para sua ex-namorada, a traficante Alex (Laura Prepon) — que não vê há mais de uma década. Piper troca a sua vida confortável de Nova York, com o noivo Larry (Jason Biggs), pelo macacão laranja, e cumpre sua sentença na Penitenciária Feminina de Litchfield. Para sobreviver, ela precisa aprender a conviver com as outras detentas, como Red (Kate Mulgrew), Nicky (Natasha Lyonne), Taystee (Danielle Brooks) e Crazy Eyes (Uzo Aduba). O que Piper não espera é encontrar a ex cumprindo pena no mesmo lugar. ",
+ }
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+function exibeDetalhes(item) {
+  document.getElementById("imagem_detalhes").src = item.imagem;
+  document.getElementById("serie_detalhes").innerHTML = item.serie;
+  document.getElementById("criado_por_detalhes").innerHTML = item.criado_por;
+  document.getElementById("elenco_detalhes").innerHTML = item.elenco;
+  document.getElementById("pais_detalhes").innerHTML = item.pais;
+  document.getElementById("genero_detalhes").innerHTML = item.genero;
+  document.getElementById("status_detalhes").innerHTML = item.status;
+  document.getElementById("duracao_detalhes").innerHTML = item.duracao;
+  document.getElementById("n_temporadas_detalhes").innerHTML = item.n_temporadas;
+  document.getElementById("emissora_detalhes").innerHTML = item.emissoras;
+  document.getElementById("sinopse_detalhes").innerHTML = item.sinopse;
+}
+var serie = getParameterByName("serie");
+exibeDetalhes(series[serie]);
